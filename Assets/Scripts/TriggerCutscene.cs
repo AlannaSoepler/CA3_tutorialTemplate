853e521f100cab7efa5e4ddd5036ffb1ad36bbd1
AlannaSoepler <91547825+AlannaSoepler@users.noreply.github.com>
2023-01-03 17:18:56 +0000

    Updated comment on the TriggerCutscene script

--- Assets/Scripts/TriggerCutscene.cs
+++ Assets/Scripts/TriggerCutscene.cs
@@ -8,31 +8,32 @@ public class TriggerCutscene : MonoBehaviour
 {
     //Required to select which timeline you want to use. 
     //You can input the timeline you want to use in Unity
-    public PlayableDirector timeline;
+    public PlayableDirector Timeline;
     //The main camera for this example
     public GameObject Camera1;
-    //The camera that is used in the cutscene
+    //The camera that is used in the cutscene.
     public GameObject Camera2;
 
     // Start is called before the first frame update
     void Start()
     {
-        //To initiate the timeline feature.
-        //You should see on the timeline object, in Unity, that a component called Playable Director has been added. 
-        timeline.GetComponent<PlayableDirector>();
+        //To initiate the Timeline feature.
+        //Returns the component of type PlayableDirector
+        //You can find this component in the Inspector of the Game Object associated with the Timeline. 
+        Timeline.GetComponent<PlayableDirector>();
     }
 
     // Triggered when the player interacts with the GameObject
     private void OnTriggerEnter(Collider other)
     {
-        //If the player object tagged player hit this triggerable GameObject
+        //If the player object tagged player hit this triggerable GameObject do this:
         if(other.tag == "Player"){
             //Turn the cutscene camera on
             Camera2.SetActive(true);
             //Disable the main camera
             Camera1.SetActive(false);
-            //Play the selected timeline
-            timeline.Play();  
+            //Play the selected Timeline
+            Timeline.Play();  
         }
 
     }
